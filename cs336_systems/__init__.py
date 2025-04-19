39925ec8d1cfd8373cda6af35f1d041243d79067
Ayush Alag <aalag@stanford.edu>
2025-04-19 16:59:59 -0700

    fixes

--- cs336_systems/__init__.py
+++ cs336_systems/__init__.py
@@ -1,3 +1,4 @@
 import importlib.metadata
+from .cs336_basics import BasicsTransformerLM
 
 __version__ = importlib.metadata.version("cs336-systems")
--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -1,6 +1,6 @@
 import argparse
 from enum import Enum
-from cs336_basics.cs336_basics import BasicsTransformerLM
+from cs336_basics import BasicsTransformerLM
 import torch
 from timeit import default_timer as timer
 
@@ -21,57 +21,56 @@ def create_model(args):
 
 def benchmark_model(model, args):
     model.eval()
-    with torch.no_grad():
-        def run_forward(model, args):
-            input_ids = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length), device=args.device)
-            output = model(input_ids).mean()
-            return output
+    def run_forward(model, args):
+        input_ids = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length), device=args.device)
+        output = model(input_ids).mean()
+        return output
 
-        def run_backward(output):
-            output.backward()
+    def run_backward(output):
+        output.backward()
 
-        # some warmup steps
-        for _ in range(args.warmup_steps):
-            output = run_forward(model, args)
-            if args.run_backward:
-                run_backward(output)
+    # some warmup steps
+    for _ in range(args.warmup_steps):
+        output = run_forward(model, args)
+        if args.run_backward:
+            run_backward(output)
 
-        if torch.cuda().is_available():
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+
+    # benchmark steps
+    forward_times = []
+    backward_times = []
+    for _ in range(args.n_steps):
+        start_time = timer()
+
+        output = run_forward(model, args)
+
+        if torch.cuda.is_available():
             torch.cuda.synchronize()
 
-        # benchmark steps
-        forward_times = []
-        backward_times = []
-        for _ in range(args.n_steps):
-            start_time = timer()
-
-            run_forward(model, args)
-
-            if torch.cuda().is_available():
-                torch.cuda.synchronize()
-
-            forward_times.append((timer() - start_time) * 1000)
-
-            if args.run_backward:
-                start_time = timer()
-                run_backward(model, args)
-
-                if torch.cuda().is_available():
-                    torch.cuda.synchronize()
-
-                backward_times.append((timer() - start_time) * 1000)
-
-        mean_forward_time = sum(forward_times) / len(forward_times)
-        std_forward_time = (sum((x - mean_forward_time) ** 2 for x in forward_times) / len(forward_times)) ** 0.5
-        print(f"Forward pass: {mean_forward_time:.2f} ± {std_forward_time:.2f} ms")
+        forward_times.append((timer() - start_time) * 1000)
 
         if args.run_backward:
-            mean_backward_time = sum(backward_times) / len(backward_times)
-            std_backward_time = (sum((x - mean_backward_time) ** 2 for x in backward_times) / len(backward_times)) ** 0.5
-            print(f"Backward pass: {mean_backward_time:.2f} ± {std_backward_time:.2f} ms")
-            return mean_forward_time, mean_backward_time
+            start_time = timer()
+            run_backward(output)
 
-        return mean_forward_time, None
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+
+            backward_times.append((timer() - start_time) * 1000)
+
+    mean_forward_time = sum(forward_times) / len(forward_times)
+    std_forward_time = (sum((x - mean_forward_time) ** 2 for x in forward_times) / len(forward_times)) ** 0.5
+    print(f"Forward pass: {mean_forward_time:.2f} ± {std_forward_time:.2f} ms")
+
+    if args.run_backward:
+        mean_backward_time = sum(backward_times) / len(backward_times)
+        std_backward_time = (sum((x - mean_backward_time) ** 2 for x in backward_times) / len(backward_times)) ** 0.5
+        print(f"Backward pass: {mean_backward_time:.2f} ± {std_backward_time:.2f} ms")
+        return mean_forward_time, mean_backward_time
+
+    return mean_forward_time, None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Benchmark a specified model")
@@ -89,7 +88,7 @@ if __name__ == "__main__":
     # benchmarking args
     parser.add_argument("--warmup_steps", type=int, default=12)
     parser.add_argument("--n_steps", type=int, default=100)
-    parser.add_argument("--run_backward", type=bool, default=False)
+    parser.add_argument("--run_backward", action="store_true")
     # device args
     parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu")
     args = parser.parse_args()
